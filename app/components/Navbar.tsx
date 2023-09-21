293008f54ac009744a3d01d2cdc48392208acaba
Pellegrino Smiling <n.pellegrino@smiling.video>
2023-09-21 17:44:12 +0200

    Navbar setup

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,39 @@
+import Link from "next/link";
+import { FaFacebook, FaGithub, FaLinkedin } from "react-icons/fa";
+
+export default function Navbar() {
+    return (
+        <nav className="bg-slate-600 p-4 sticky top-0 drop-shadow-xl z-10">
+            <div className="prose prose-xl mx-auto flex justify-between flex-col sm:flex-row">
+                <h1 className="text-3xl font-bold text-white grid place-content-center mb-2 md:mb-0">
+                    <Link
+                        href="/"
+                        className="text-white/90 no-underline hover:text-white"
+                    >
+                        Nicola Pellegrino
+                    </Link>
+                </h1>
+                <div className="flex flex-row justify-center sm:justify-evenly align-middle gap-4 text-white text-4xl lg:text-5xl">
+                    <Link
+                        className="text-white/90 hover:text-white"
+                        href="https://it.linkedin.com/in/nicola-pellegrino27"
+                    >
+                        <FaLinkedin />
+                    </Link>
+                    <Link
+                        className="text-white/90 hover:text-white"
+                        href="https://github.com/Nolekor"
+                    >
+                        <FaGithub />
+                    </Link>
+                    <Link
+                        className="text-white/90 hover:text-white"
+                        href="https://www.facebook.com/NicoPelle27"
+                    >
+                        <FaFacebook />
+                    </Link>
+                </div>
+            </div>
+        </nav>
+    );
+}
